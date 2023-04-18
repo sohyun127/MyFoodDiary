f347d92d83ce1a2fa2581f92be48bb7ecdf5f0a0
Sohyun <so405@naver.com>
2023-04-19 02:45:14 +0900

    Feat: Signup xml UI 구성

--- app/src/main/java/com/example/myfooddiary/SigninActivity.java
+++ app/src/main/java/com/example/myfooddiary/SigninActivity.java
@@ -3,12 +3,28 @@ package com.example.myfooddiary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class SigninActivity extends AppCompatActivity {
+import com.example.myfooddiary.databinding.ActivitySigninBinding;
+
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivitySigninBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        binding= ActivitySigninBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+
+
+
+
     }
 }
