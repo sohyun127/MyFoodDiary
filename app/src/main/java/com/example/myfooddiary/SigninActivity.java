5f38c31e679c30819fbeb095a62d5203f2db216e
Sohyun <so405@naver.com>
2023-04-19 04:08:40 +0900

    Feat: 회원 가입 후 로그인 기능

--- app/src/main/java/com/example/myfooddiary/SigninActivity.java
+++ app/src/main/java/com/example/myfooddiary/SigninActivity.java
@@ -5,36 +5,68 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.myfooddiary.databinding.ActivitySigninBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivitySigninBinding binding;
+    private Intent it;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivitySigninBinding.inflate(getLayoutInflater());
+        binding = ActivitySigninBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        TextView signUp = binding.tvSigninSignup;
-        signUp.setOnClickListener(this);
+        it = getIntent();
+
+        TextView tvSignUp = binding.tvSigninSignup;
+        tvSignUp.setOnClickListener(this);
+
+        Button btnLogin = binding.btnSigninLogin;
+        btnLogin.setOnClickListener(this);
     }
 
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()){
+        switch (v.getId()) {
             case R.id.tv_signin_signup:
-                Intent it = new Intent(this,SignupActivity.class);
-                startActivity(it); //회원가입 페이지로 이동
+                Intent signup = new Intent(this, SignupActivity.class);
+                startActivity(signup); //회원가입 페이지로 이동
                 finish();
+            case R.id.btn_signin_login:
+                if (checkLogin()) {
+                    Intent main = new Intent(this, MainActivity.class);
+                    Snackbar.make(binding.getRoot(), "로그인에 성공했습니다.", Snackbar.LENGTH_SHORT).show();
+                    startActivity(main);
+                    finish();
+                } else {
+                    Snackbar.make(binding.getRoot(), "로그인에 실패했습니다.", Snackbar.LENGTH_SHORT).show();
+                }
+
         }
 
 
+    }
 
+    //아이디, 비밀번호 확인 함수
+    public boolean checkLogin() {
+
+        String id = it.getStringExtra("id");
+        String pw = it.getStringExtra("pw");
+
+        if (binding.etSigninId.getText().toString().equals(id)&& binding.etSigninPw.getText().toString().equals(pw)) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
+
+
 }
--- app/src/main/java/com/example/myfooddiary/SignupActivity.java
+++ app/src/main/java/com/example/myfooddiary/SignupActivity.java
@@ -2,13 +2,43 @@ package com.example.myfooddiary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class SignupActivity extends AppCompatActivity {
+
+import com.example.myfooddiary.databinding.ActivitySignupBinding;
+import com.google.android.material.snackbar.Snackbar;
+
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ActivitySignupBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signup);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Button btnComplete = binding.btnSignupComplete;
+        btnComplete.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_signup_complete:
+                Intent it = new Intent(this, SigninActivity.class);
+                Snackbar.make(binding.getRoot(), "회원가입에 성공했습니다.", Snackbar.LENGTH_SHORT).show();
+
+                it.putExtra("id", binding.etSignupId.getText().toString());
+                it.putExtra("pw", binding.etSignupPw.getText().toString());
+                startActivity(it);
+                finish();
+        }
+
+
     }
 }
