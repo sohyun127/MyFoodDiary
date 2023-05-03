f074f92fabbd3106abc7bf5b528c7995f95bd19d
Sohyun <so405@naver.com>
2023-05-03 13:45:42 +0900

    Add: 재료 OCR activity

---
+++ app/src/main/java/com/example/myfooddiary/Home/Ingredient/IngredientOcrActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfooddiary.Home.Ingredient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myfooddiary.R;
+
+public class IngredientOcrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_ocr);
+    }
+}
