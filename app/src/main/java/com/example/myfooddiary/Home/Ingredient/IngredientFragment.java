f9b1cb787a56c356b979bc09faf67b8fd8e4c1de
Sohyun <so405@naver.com>
2023-05-03 16:01:42 +0900

    Feat: OCR 텍스트 인식 기능

--- app/src/main/java/com/example/myfooddiary/Home/Ingredient/IngredientFragment.java
+++ app/src/main/java/com/example/myfooddiary/Home/Ingredient/IngredientFragment.java
@@ -1,9 +1,11 @@
 package com.example.myfooddiary.Home.Ingredient;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,7 +13,7 @@ import androidx.fragment.app.Fragment;
 
 import com.example.myfooddiary.databinding.FragmentIngredientBinding;
 
-public class IngredientFragment extends Fragment {
+public class IngredientFragment extends Fragment implements View.OnClickListener {
 
     private FragmentIngredientBinding binding;
 
@@ -26,11 +28,25 @@ public class IngredientFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        Button btn = binding.button;
+        btn.setOnClickListener(this);
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding=null;
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent it = new Intent(getActivity() ,IngredientOcrActivity.class);
+
+        startActivity(it);
+
+
     }
 }
--- app/src/main/java/com/example/myfooddiary/Home/Ingredient/IngredientOcrActivity.java
+++ app/src/main/java/com/example/myfooddiary/Home/Ingredient/IngredientOcrActivity.java
@@ -1,16 +1,133 @@
 package com.example.myfooddiary.Home.Ingredient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 
-import com.example.myfooddiary.R;
+import com.example.myfooddiary.databinding.ActivityIngredientOcrBinding;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 public class IngredientOcrActivity extends AppCompatActivity {
+
+    private ActivityIngredientOcrBinding binding;
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+    final int RequestCameraPermissionID = 1001;
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case RequestCameraPermissionID: {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ingredient_ocr);
+        binding = ActivityIngredientOcrBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        cameraView = binding.svIngredientOcr;
+        textView = binding.tvIngredientOcr;
+
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        if (!textRecognizer.isOperational()) {
+
+            Log.w("IngredientOcrActivity", "Detactor dependecies are not yet available");
+        } else {
+            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280, 1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(@NonNull SurfaceHolder holder) {
+
+                    try {
+                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                            ActivityCompat.requestPermissions(IngredientOcrActivity.this,
+                                    new String[]{Manifest.permission.CAMERA},
+                                    RequestCameraPermissionID);
+                            return;
+                        }
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                @Override
+                public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+                }
+
+                @Override
+                public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                    cameraSource.stop();
+                }
+            });
+
+            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+                @Override
+                public void release() {
+
+                }
+
+                @Override
+                public void receiveDetections(@NonNull Detector.Detections<TextBlock> detections) {
+
+                    final SparseArray<TextBlock> items = detections.getDetectedItems();
+                    if(items.size()!=0){
+
+                        textView.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                StringBuilder stringBuilder = new StringBuilder();
+                                for(int i=0;i< items.size();++i){
+
+                                    TextBlock item= items.valueAt(i);
+                                    stringBuilder.append(item.getValue());
+                                    stringBuilder.append("\n");
+                                }
+                                textView.setText(stringBuilder.toString());
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
     }
 }
