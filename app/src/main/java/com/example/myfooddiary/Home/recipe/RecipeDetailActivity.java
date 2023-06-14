853455b49113ae79f3d735b390ccca878706edb2
Sohyun <so405@naver.com>
2023-06-14 17:48:27 +0900

    Feat: 레시피 상세페이지 레시피 출력 조건 추가

--- app/src/main/java/com/example/myfooddiary/Home/recipe/RecipeDetailActivity.java
+++ app/src/main/java/com/example/myfooddiary/Home/recipe/RecipeDetailActivity.java
@@ -30,7 +30,6 @@ public class RecipeDetailActivity extends AppCompatActivity {
     private ArrayList<Recipe> arrayList;
 
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,16 +37,16 @@ public class RecipeDetailActivity extends AppCompatActivity {
         binding = ActivityRecipeDetailBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setView();
+        setData();
         setClickEventOnToolBar();
 
     }
 
-    private void setView(){
+    private void setData() {
 
         arrayList = new ArrayList<>();
         database = FirebaseDatabase.getInstance();
-        int position = getIntent().getIntExtra("id",0);
+
 
         databaseReference = database.getReference("recipe");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -62,21 +61,7 @@ public class RecipeDetailActivity extends AppCompatActivity {
 
                 }
 
-                Glide.with(binding.getRoot()).load(arrayList.get(position).getUrl()).into(binding.ivRecipeDetail);
-                binding.tvRecipeTitle.setText(arrayList.get(position).getName());
-                binding.tvRecipeKcal.setText(arrayList.get(position).getKcal());
-                binding.tvRecipeIngredient.setText(arrayList.get(position).getIngredient());
-                binding.tvRecipeOrder.append("1. " + arrayList.get(position).getRecipe1());
-                binding.tvRecipeOrder.append("\n\n2. " + arrayList.get(position).getRecipe2());
-                binding.tvRecipeOrder.append("\n\n3. " + arrayList.get(position).getRecipe3());
-                binding.tvRecipeOrder.append("\n\n4. " + arrayList.get(position).getRecipe4());
-                binding.tvRecipeOrder.append("\n\n5. " + arrayList.get(position).getRecipe5());
-                binding.tvRecipeOrder.append("\n\n6. " + arrayList.get(position).getRecipe6());
-                binding.tvRecipeOrder.append("\n\n7. " + arrayList.get(position).getRecipe7());
-                binding.tvRecipeOrder.append("\n\n8. " + arrayList.get(position).getRecipe8());
-                binding.tvRecipeOrder.append("\n\n9. " + arrayList.get(position).getRecipe9());
-                binding.tvRecipeOrder.append("\n\n10. " + arrayList.get(position).getRecipe10());
-
+                setView();
 
             }
 
@@ -89,6 +74,47 @@ public class RecipeDetailActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    private void setView() {
+
+        int position = getIntent().getIntExtra("id", 0);
+
+        Glide.with(binding.getRoot()).load(arrayList.get(position).getUrl()).into(binding.ivRecipeDetail);
+        binding.tvRecipeTitle.setText(arrayList.get(position).getName());
+        binding.tvRecipeKcal.setText(arrayList.get(position).getKcal());
+        binding.tvRecipeIngredient.setText(arrayList.get(position).getIngredient());
+
+        if (!arrayList.get(position).getRecipe1().isBlank()) {
+            binding.tvRecipeOrder.append("1. " + arrayList.get(position).getRecipe1());
+        }
+        if (!arrayList.get(position).getRecipe2().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n2. " + arrayList.get(position).getRecipe2());
+        }
+        if (!arrayList.get(position).getRecipe3().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n3. " + arrayList.get(position).getRecipe3());
+        }
+        if (!arrayList.get(position).getRecipe4().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n4. " + arrayList.get(position).getRecipe4());
+        }
+        if (!arrayList.get(position).getRecipe5().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n5. " + arrayList.get(position).getRecipe5());
+        }
+        if (!arrayList.get(position).getRecipe6().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n6. " + arrayList.get(position).getRecipe6());
+        }
+        if (!arrayList.get(position).getRecipe7().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n7. " + arrayList.get(position).getRecipe7());
+        }
+        if (!arrayList.get(position).getRecipe8().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n8. " + arrayList.get(position).getRecipe8());
+        }
+        if (!arrayList.get(position).getRecipe9().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n9. " + arrayList.get(position).getRecipe9());
+        }
+        if (!arrayList.get(position).getRecipe10().isBlank()) {
+            binding.tvRecipeOrder.append("\n\n10. " + arrayList.get(position).getRecipe10());
+        }
     }
 
 
@@ -107,9 +133,6 @@ public class RecipeDetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-
-
 
 
 }
